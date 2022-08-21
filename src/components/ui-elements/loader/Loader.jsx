12208ea94545ea3d5c6cd1e3799793de6103e24e
HardeepsinhSodha <hardeepsinhsodha99@gmail.com>
2022-08-21 12:46:23 +0530

    update loader and scrollbar style

--- src/components/ui-elements/loader/Loader.jsx
+++ src/components/ui-elements/loader/Loader.jsx
@@ -3,13 +3,15 @@ import './loader.css'
 
 export default function Loader() {
   return (
-    <div className="gooey">
-        <span className="dot"></span>
-        <div className="dots">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+    <div className='h-screen'>
+      <div className="gooey rounded-lg bg-black dark:bg-white">
+          <span className="dot bg-white dark:bg-black"></span>
+          <div className="dots ">
+              <span className='bg-white dark:bg-black'></span>
+              <span className='bg-white dark:bg-black'></span>
+              <span className='bg-white dark:bg-black'></span>
+          </div>
+      </div>
     </div>
   )
 }
